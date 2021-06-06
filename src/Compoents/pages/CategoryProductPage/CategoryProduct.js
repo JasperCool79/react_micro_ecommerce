049670bf9eecc42e21a59de7c6fad90717e5d08c
JasperCool79 <jaspercool.ygn@gmail.com>
2021-06-06 18:46:45 +0630

    package.json

--- src/Compoents/pages/CategoryProductPage/CategoryProduct.js
+++ src/Compoents/pages/CategoryProductPage/CategoryProduct.js
@@ -49,9 +49,6 @@ export default function CategoryProduct() {
         receivedData();
     }
     async function fetchData() {
-        axios.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest';
-        axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*',
-        axios.defaults.headers.post['crossDomain'] = true;
         let response = await axios.get(`${URL}/get_products_category_id/`, {
             params: {
                 category_id: id
