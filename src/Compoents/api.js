ab6672b8fdd8a41efbc070bebbed8835d63fc85e
JasperCool79 <jaspercool.ygn@gmail.com>
2021-06-11 16:07:45 +0630

    cors freeboard

--- src/Compoents/api.js
+++ src/Compoents/api.js
@@ -1,5 +1,6 @@
 import 'cors-anywhere';
 // export const URL = 'http://127.0.0.1:8000/api';
+
 // export const URL = 'https://cors-anywhere.herokuapp.com/https://mico-ecommerce.herokuapp.com/api';
 export const URL = 'https://thingproxy.freeboard.io/fetch/https://mico-ecommerce.herokuapp.com/api';
 
